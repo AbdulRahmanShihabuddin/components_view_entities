d0eed385a1be2f7d4739f395da9aea6423767f61
Lafuon <abdul.rahman.iv7@gmail.com>
2025-02-18 11:28:09 +0530

    Done

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,21 @@
 // write the book component code here
+const BookCard = ({ book }) => {
+    return (
+        <div style={{
+            border: '10px solid green',
+            width: "50%",
+            borderRadius:"10px",
+            justifyContent:"center",
+            textAlign: 'center',
+            padding: '10px',
+            margin: '10px',
+        }}>
+            <img src={book.image} alt={book.name} style={{ width: '100%', borderRadius: '5px' }} />
+            <h3>{book.name}</h3>
+            <p>{book.genre}</p>
+            <p>{book.author}</p>
+        </div>
+    );
+};
+
+export default BookCard;
